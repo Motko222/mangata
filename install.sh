e96bf4dcb5723bfedc362ffde6c81d65fc0b97d6
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-23 22:29:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,15 +16,15 @@ read -p "ws rpc url? " ws
 read -p "wallet password? " pwd
 
 http=$(cat /root/eigenlayer/cli/operator/config/operator-config.yaml | grep eth_rpc_url | awk '{print $2}' )
-ecdsa=$(cat /root/eigenlayer/cli/operator/config/operator-config.yaml | grep private_key_store_path | awk -F "operator_keys/" '{print $NF}' )
-bls=$(cat /root/eigenlayer/cli/operator/config/operator-config.yaml | grep bls_private_key_store_path | awk -F "operator_keys/" '{print $NF}' )
+ecdsa=$(cat /root/eigenlayer/cli/operator/config/operator-config.yaml | grep -w private_key_store_path: | awk -F "operator_keys/" '{print $NF}' )
+bls=$(cat /root/eigenlayer/cli/operator/config/operator-config.yaml | grep -w bls_private_key_store_path: | awk -F "operator_keys/" '{print $NF}' )
 
-sed 's/ETH_RPC_URL=/ETH_RPC_URL=$http/' .env
-sed 's/ETH_WS_URL=/ETH_WS_URL=$ws/' .env
-sed 's/~/.eigenlayer/operator_keys/ecdsa.key.json/$ecdsa/' .env
-sed 's/ecdsa.key.json/$ecsda/' .env
-sed 's/bls.key.json/$bls/' .env
-sed 's/ECDSA_KEY_PASSWORD=/ECDSA_KEY_PASSWORD=$pwd/' .env
-sed 's/BLS_KEY_PASSWORD=/BLS_KEY_PASSWORD=$pwd/' .env
+sed -i 's/ETH_RPC_URL=/ETH_RPC_URL=$http/' .env
+sed -i 's/ETH_WS_URL=/ETH_WS_URL=$ws/' .env
+sed -i 's/~/.eigenlayer/operator_keys/ecdsa.key.json/$ecdsa/' .env
+sed -i 's/ecdsa.key.json/$ecsda/' .env
+sed -i 's/bls.key.json/$bls/' .env
+sed -i 's/ECDSA_KEY_PASSWORD=/ECDSA_KEY_PASSWORD=$pwd/' .env
+sed -i 's/BLS_KEY_PASSWORD=/BLS_KEY_PASSWORD=$pwd/' .env
 
 ./run.sh opt-in
